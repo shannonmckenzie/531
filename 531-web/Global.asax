7591b1224c06e8a7f32679926d43ab91601d9b24
Shannon McKenzie <shannon@dorgersoft.com>
2015-02-16 22:30:15 -0600

    initial commit of empty mvc project

---
+++ 531-web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_531_web.MvcApplication" Language="C#" %>
---
+++ 531-web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_531_web.Startup))]
+namespace _531_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
